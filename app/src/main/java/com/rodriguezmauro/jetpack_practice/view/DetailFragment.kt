aa1d05c308713b8d4ac7787949ada673248eefb9
MauroRodriguez94 <mauro.rodriguez@mercadolibre.com>
2019-12-07 17:40:15 -0300

    added calls to fragments

--- app/src/main/java/com/rodriguezmauro/jetpack_practice/view/DetailFragment.kt
+++ app/src/main/java/com/rodriguezmauro/jetpack_practice/view/DetailFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.rodriguezmauro.jetpack_practice.R
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
     override fun onCreateView(
@@ -15,5 +17,14 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonList.setOnClickListener {
+            val action = DetailFragmentDirections.actionListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
