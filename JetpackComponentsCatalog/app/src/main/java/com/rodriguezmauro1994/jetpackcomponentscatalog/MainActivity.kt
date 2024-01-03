dd756bbc2120fc00ab83911850d329933d09c92b
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2024-01-03 20:44:14 -0300

    added components

--- JetpackComponentsCatalog/app/src/main/java/com/rodriguezmauro1994/jetpackcomponentscatalog/MainActivity.kt
+++ JetpackComponentsCatalog/app/src/main/java/com/rodriguezmauro1994/jetpackcomponentscatalog/MainActivity.kt
@@ -9,14 +9,18 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -24,11 +28,16 @@ import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TriStateCheckbox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
@@ -41,6 +50,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -60,6 +70,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
+                    //Texts con estado
                     /*var myText by remember {
                         mutableStateOf("Pepe")
                     }
@@ -68,7 +80,28 @@ class MainActivity : ComponentActivity() {
                             myText = it
                         }
                     }*/
-                    DefaultPreview()
+
+                    //Listado de Checkboxes con estado
+                    /*val myOptions = getOptions(titles = listOf("Mau", "Ejemplo", "Pikachu"))
+
+                    Column {
+                        MyTriStatus()
+                        myOptions.forEach {
+                            myCheckboxWithTextCompleted(it)
+                        }
+                        MyCheckboxWithText()
+                    }*/
+
+                    var selected by remember {
+                        mutableStateOf("Mau")
+                    }
+
+                    Column {
+                        MyRadioButtonList(selected) {
+                            selected = it
+                        }
+                    }
+
                 }
             }
         }
@@ -79,8 +112,160 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     JetpackComponentsCatalogTheme {
-        MyProgressAdvance()
+        MyRadioButton()
     }
+}
+
+@Composable
+fun MyRadioButtonList(name: String, onItemSelected: (String) -> Unit) {
+    Column(Modifier.fillMaxWidth()) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(
+                selected = name == "Mauro",
+                onClick = { onItemSelected("Mauro") }
+            )
+            Text(text = "Mauro")
+        }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(
+                selected = name == "Fulano",
+                onClick = { onItemSelected("Fulano") }
+            )
+            Text(text = "Fulano")
+        }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(
+                selected = name == "Mengano",
+                onClick = { onItemSelected("Mengano") }
+            )
+            Text(text = "Mengano")
+        }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(
+                selected = name == "Pepe",
+                onClick = { onItemSelected("Pepe") }
+            )
+            Text(text = "Pepe")
+        }
+    }
+}
+
+@Composable
+fun MyRadioButton() {
+    var selected by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+        RadioButton(
+            selected = selected,
+            onClick = {
+                selected = true
+            },
+            enabled = true,
+            colors = RadioButtonDefaults.colors(
+                selectedColor = Color.Red,
+                unselectedColor = Color.Yellow,
+                disabledSelectedColor = Color.Green,
+                disabledUnselectedColor = Color.Green
+            )
+        )
+        Text(text = "Ejemplo 1")
+    }
+}
+
+@Composable
+fun MyTriStatus() {
+    var status by rememberSaveable {
+        mutableStateOf(ToggleableState.Off)
+    }
+    
+    TriStateCheckbox(state = status, onClick = {
+        status = when(status) {
+            ToggleableState.On -> ToggleableState.Off
+            ToggleableState.Off -> ToggleableState.Indeterminate
+            ToggleableState.Indeterminate -> ToggleableState.On
+        }
+    })
+}
+
+@Composable
+fun getOptions(titles: List<String>): List<CheckInfo> {
+    return titles.map {
+        var status by rememberSaveable { mutableStateOf(false) }
+        CheckInfo(
+            title = it,
+            selected = status,
+            onCheckedChange = { myNewStatus ->
+                status = myNewStatus
+            }
+        )
+    }
+}
+
+@Composable
+fun myCheckboxWithTextCompleted(checkInfo: CheckInfo) {
+    Row(Modifier.padding(8.dp), verticalAlignment = Alignment.CenterVertically) {
+        Checkbox(checked = checkInfo.selected, onCheckedChange = { checkInfo.onCheckedChange(!checkInfo.selected) })
+        Text(text = checkInfo.title)
+    }
+}
+
+@Composable
+fun MyCheckboxWithText() {
+    var state by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    Row(Modifier.padding(8.dp), verticalAlignment = Alignment.CenterVertically) {
+        Checkbox(checked = state, onCheckedChange = { state = !state })
+        Text(text = "Ejemplo 1")
+    }
+}
+
+@Composable
+fun myCheckbox() {
+    var state by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    Checkbox(
+        checked = state,
+        onCheckedChange = {
+            state = !state
+        },
+        enabled = true,
+        colors = CheckboxDefaults.colors(
+            checkedColor = Color.Red,
+            uncheckedColor = Color.Yellow,
+            checkmarkColor = Color.Blue
+        )
+    )
+}
+
+@Composable
+fun MySwitch() {
+    var state by rememberSaveable {
+        mutableStateOf(true)
+    }
+
+    Switch(
+        checked = state,
+        onCheckedChange = {
+            state = !state
+        },
+        enabled = true,
+        colors = SwitchDefaults.colors(
+            uncheckedThumbColor = Color.Red,
+            uncheckedTrackColor = Color.Magenta,
+            checkedThumbColor =  Color.Green,
+            checkedTrackColor = Color.Cyan,
+            disabledCheckedThumbColor = Color.Yellow,
+            disabledCheckedTrackColor = Color.Yellow,
+            disabledUncheckedThumbColor = Color.Yellow,
+            disabledUncheckedTrackColor = Color.Yellow
+        )
+    )
 }
 
 @Composable
